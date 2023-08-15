2ba30342cf1e2fb5d364d72cebac43e05f4f4de9
Emilly-xavier <emilly.xavier@estudante.ifms.edu.br>
2023-08-15 08:11:11 -0400

    Atualização do HEADER

--- src/componentes/header/Header.tsx
+++ src/componentes/header/Header.tsx
@@ -10,7 +10,7 @@ function Header(){
             <nav>
                 <ul>
                     <li>
-                        <a href="#">Home</a>
+                        <a href="#">Pagina Inicial</a>
                     </li>
                     <li>
                         <a href="#">Sobre</a>
