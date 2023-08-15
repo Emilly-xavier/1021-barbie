45bdeb3024fcbe52d3f04ee051c160534aac3766
EMILLY SANTOS XAVIER <07010080160@ifms.edu.br>
2023-08-15 07:33:51 -0400

    primeiro commit

---
+++ src/componentes/footer/Footer.tsx
@@ -0,0 +1,32 @@
+import './footer.css'
+function Footer(){
+    return(
+    <footer className="rodape">
+        <div className='d1'>
+            <h1>Barbie</h1>
+            <p>Barbie, tudo o que você quer ser!</p>
+        </div>
+        <div className='d1'>
+            <a href="#">Product</a>
+            <a href="#">Mobile apps</a>
+            <a href="#">Database Designer</a>
+            <a href="#">Solutions</a>
+        </div>
+
+        <div className='d1'>
+            <a href="#">Company</a>
+            <a href="#">Careers</a>
+            <a href="#">Patner program</a>
+            <a href="#">Find a Partner</a>
+        </div>
+
+        <div className='d1'>
+            <a href="#">Community</a>
+            <a href="#">Discord Community</a>
+            <a href="#">Documentation</a>
+            <a href="#">Comparison</a>
+        </div>
+</footer>
+    )
+}
+export default Footer
---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,30 @@
+import './Header.css'
+
+function Header(){
+    return(
+        <header>
+            <h1>
+                Barbie
+            </h1>
+                
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#">Noticias</a>
+                    </li>
+                    <li>
+                        <a href="#">Contato</a>
+                    </li>
+                </ul>
+            </nav>
+            
+        </header>
+    )
+}
+export default Header
