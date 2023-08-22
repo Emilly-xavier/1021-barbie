bdb72d2fd48ca5a6c28cffca1b470631863a7018
Emilly-xavier <emilly.xavier@estudante.ifms.edu.br>
2023-08-22 08:06:32 -0400

    Iniciando a programação do footer

--- src/componentes/footer/Footer.tsx
+++ src/componentes/footer/Footer.tsx
@@ -1,32 +1,11 @@
-import './footer.css'
-function Footer(){
+import './Footer.css'
+import Logo from '../../assets/Barbie_Logo.png'
+
+export default function Footer(){
     return(
-    <footer className="rodape">
-        <div className='d1'>
-            <h1>Barbie</h1>
-            <p>Barbie, tudo o que você quer ser!</p>
-        </div>
-        <div className='d1'>
-            <a href="#">Product</a>
-            <a href="#">Mobile apps</a>
-            <a href="#">Database Designer</a>
-            <a href="#">Solutions</a>
-        </div>
+        <footer>
+            <img src={Logo} alt="" />
 
-        <div className='d1'>
-            <a href="#">Company</a>
-            <a href="#">Careers</a>
-            <a href="#">Patner program</a>
-            <a href="#">Find a Partner</a>
-        </div>
-
-        <div className='d1'>
-            <a href="#">Community</a>
-            <a href="#">Discord Community</a>
-            <a href="#">Documentation</a>
-            <a href="#">Comparison</a>
-        </div>
-</footer>
+        </footer>
     )
 }
-export default Footer
